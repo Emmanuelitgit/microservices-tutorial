d6401b7d5c2880920dc08f1cd4e839b2c54f0cfc
Emmanuel Yidana <EmmanuelYi@stlghana.com>
2025-05-22 18:08:50 +0000

    added kafka

---
+++ student-service/src/main/java/com/student_service/student_service/config/kafka/Producer.java
@@ -0,0 +1,15 @@
+package com.student_service.student_service.config.kafka;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class Producer {
+
+}
